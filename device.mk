3e42d528a592636ed22452aa4133c897b50d7a6b
shamith <shamith@firehound.org>
2018-10-26 11:50:17 +0530

    Initial changes for Validus

--- device.mk
+++ device.mk
@@ -15,11 +15,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    $(LOCAL_PATH)/overlay
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
